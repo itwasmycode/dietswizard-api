4f0e8f8b492a21aa09648edd6b71e3331e9ba9d1
itwasmycode <kemalemrecolak@gmail.com>
2023-08-05 23:31:35 +0300

    testing map purpose28

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "image_uri" {
+  type = string
+  # You can add an optional default value if needed:
+  default = ""
+}
+
+variable "postgre_id" {
+  type = string
+  # You can add an optional default value if needed:
+  default = ""
+}
+
+variable "postgre_pw" {
+  type = string
+  # You can add an optional default value if needed:
+  default = ""
+}
