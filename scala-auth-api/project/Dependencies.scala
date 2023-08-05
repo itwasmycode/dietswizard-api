892ba27e6ffb000a1c28a90ca3c97d0d86aad5ab
itwasmycode <kemalemrecolak@gmail.com>
2023-08-05 21:13:05 +0300

    testing map purpose4

--- scala-auth-api/project/Dependencies.scala
+++ scala-auth-api/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val lambdaRuntimeInterfaceClient = "com.amazonaws" % "aws-lambda-java-runtime-interface-client" % "2.3.1"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
-  lazy val playJson = "com.typesafe.play" %% "play-json" % "2.10.7"
+  lazy val playJson = "com.typesafe.play" %% "play-json" % "2.9.4"
   lazy val jwt = "com.nimbusds" % "nimbus-jose-jwt" % "9.12"
   lazy val xmlBind= "javax.xml.bind" % "jaxb-api" % "2.3.1"
 }
