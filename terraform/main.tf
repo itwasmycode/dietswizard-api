a14dc8269c0985dbd823382603d1f42459a97bcb
itwasmycode <kemalemrecolak@gmail.com>
2023-08-07 00:43:08 +0300

    unwanted vpcs removed.39

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,7 +114,7 @@ data "aws_iam_policy" "existing_lambda_ec2_policy" {
 }
 
 resource "aws_iam_policy" "lambda_ec2_policy" {
-  count = data.aws_iam_policy.existing_lambda_ec2_policy.id
+  count = data.aws_iam_policy.existing_lambda_ec2_policy[0]
 
   policy = jsonencode({
     Version = "2012-10-17"
