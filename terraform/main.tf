48a668541a548b6bd38123b08520abaf3770f4fc
itwasmycode <kemalemrecolak@gmail.com>
2023-08-07 19:23:53 +0300

    unwanted vpcs removed.77

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,14 +10,14 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.0.0/25"
-  availability_zone       = "eu-central-2a"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "my_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.0.128/25"
-  availability_zone       = "eu-central-2b"
+  availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = false
 }
 
