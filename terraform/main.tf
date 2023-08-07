b95d7e5860e42608acce949fe39a32daf675d7b1
itwasmycode <kemalemrecolak@gmail.com>
2023-08-07 04:28:52 +0300

    unwanted vpcs removed.58

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,7 +107,7 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 data "aws_iam_policy" "existing_lambda_ec2_policy" {
-  arn = "arn:aws:iam::021114833428:policy/lambda_ec2_policy_test"
+  name = "lambda_ec2_policy_test"
 }
 
 resource "aws_iam_policy" "lambda_ec2_policy" {
