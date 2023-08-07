d5a3cbcdba16b810218978f0c6c7b6ebb5e22d16
itwasmycode <kemalemrecolak@gmail.com>
2023-08-08 02:49:46 +0300

    unwanted vpcs removed.105

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "192.168.0.0/16"
+  cidr_block           = "192.168.0.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
