907671e22689db0919fafaebb1483225108533b9
itwasmycode <kemalemrecolak@gmail.com>
2023-08-07 20:17:09 +0300

    unwanted vpcs removed.82

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,34 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block          = "10.0.0.0/24"
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+    Name = "my_vpc"
+  }
 }
 
 resource "aws_subnet" "my_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.0.0/25"
+  cidr_block              = "192.168.0.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = false
+
+  tags = {
+    Name = "my_subnet_1"
+  }
 }
 
 resource "aws_subnet" "my_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.0.128/25"
+  cidr_block              = "192.168.1.0/24"
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = false
+
+  tags = {
+    Name = "my_subnet_2"
+  }
 }
 
 resource "aws_security_group" "public_sg" {
