d59460f604ebf2e7772fbc0b693300e230f3cf06
itwasmycode <kemalemrecolak@gmail.com>
2023-08-12 19:51:25 +0300

    chg

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
+  count = 0
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -52,145 +53,6 @@ resource "aws_subnet" "subnet_private_eucentral1b" {
   }
 }
 
-# Internet gateway
-resource "aws_internet_gateway" "internet_gateway_eucentral1" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "myapp-igw-eucentral1"
-  }
-}
-
-# Routes tables
-# - Public
-resource "aws_route_table" "route_table_public_eucentral1a" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway_eucentral1.id
-  }
-
-  tags = {
-    Name = "myapp-rtb-public-eucentral1a"
-  }
-}
-resource "aws_route_table" "route_table_public_eucentral1b" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway_eucentral1.id
-  }
-
-  tags = {
-    Name = "myapp-rtb-public-eucentral1b"
-  }
-}
-
-# - Private
-resource "aws_route_table" "route_table_private_eucentral1a" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway_eucentral1a.id
-  }
-
-  tags = {
-    Name = "myapp-rtb-private-eucentral1a"
-  }
-}
-resource "aws_route_table" "route_table_private_eucentral1b" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway_eucentral1b.id
-  }
-
-  tags = {
-    Name = "myapp-rtb-private-eucentral1b"
-  }
-}
-
-resource "aws_route_table_association" "route_table_association_public_eucentral1a" {
-  subnet_id      = aws_subnet.subnet_public_eucentral1a.id
-  route_table_id = aws_route_table.route_table_public_eucentral1a.id
-}
-resource "aws_route_table_association" "route_table_association_public_eucentral1b" {
-  subnet_id      = aws_subnet.subnet_public_eucentral1b.id
-  route_table_id = aws_route_table.route_table_public_eucentral1b.id
-}
-
-resource "aws_route_table_association" "route_table_association_private_eucentral1a" {
-  subnet_id      = aws_subnet.subnet_private_eucentral1a.id
-  route_table_id = aws_route_table.route_table_private_eucentral1a.id
-}
-resource "aws_route_table_association" "route_table_association_private_eucentral1b" {
-  subnet_id      = aws_subnet.subnet_private_eucentral1b.id
-  route_table_id = aws_route_table.route_table_private_eucentral1b.id
-}
-
-resource "aws_eip" "eip_eucentral1a" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.internet_gateway_eucentral1]
-  tags = {
-    Name = "myapp-eip-eucentral1a"
-  }
-}
-resource "aws_eip" "eip_eucentral1b" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.internet_gateway_eucentral1]
-  tags = {
-    Name = "myapp-eip-eucentral1b"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gateway_eucentral1a" {
-  allocation_id = aws_eip.eip_eucentral1a.id
-  subnet_id     = aws_subnet.subnet_public_eucentral1a.id
-
-  tags = {
-    Name = "myapp-ngw-eucentral1a"
-  }
-}
-resource "aws_nat_gateway" "nat_gateway_eucentral1b" {
-  allocation_id = aws_eip.eip_eucentral1b.id
-  subnet_id     = aws_subnet.subnet_public_eucentral1b.id
-
-  tags = {
-    Name = "myapp-ngw-eucentral1b"
-  }
-}
-resource "aws_default_network_acl" "default_network_acl" {
-  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
-  subnet_ids             = [aws_subnet.subnet_public_eucentral1a.id, aws_subnet.subnet_private_eucentral1a.id]
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  tags = {
-    Name = "myapp-default-network-acl"
-  }
-}
-
-
 resource "aws_db_subnet_group" "db" {
   name = "myapp-db-subnet-group"
 
@@ -206,29 +68,6 @@ resource "aws_db_subnet_group" "db" {
   }
 }
 
-
-resource "aws_default_security_group" "application-test-default-security-group" {
-  vpc_id = aws_vpc.vpc.id
-
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    # cidr_blocks = ["127.0.0.1/32"]
-  }
-
-  tags = {
-    Name = "application-test-default-security-group"
-  }
-}
 
 
 data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
@@ -253,6 +92,28 @@ resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
+resource "aws_default_security_group" "default_security_group" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    # cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  tags = {
+    Name = "lambda-default-security-group"
+  }
+}
 
 resource "aws_lambda_function" "example_lambda_test" {
   function_name = "example-lambda-test"
@@ -264,7 +125,7 @@ resource "aws_lambda_function" "example_lambda_test" {
 
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_private_eucentral1a.id]
-    security_group_ids = [aws_default_security_group.application-test-default-security-group.id]
+    security_group_ids = defaults(aws_security_gro  ups)
   }
 }
 
