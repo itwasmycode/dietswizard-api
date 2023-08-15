b84895735c4c1b00c96c3f064488573a988a70cc
itwasmycode <kemalemrecolak@gmail.com>
2023-08-15 20:26:46 +0300

    problem w smt solved2

--- terraform/main.tf
+++ terraform/main.tf
@@ -180,13 +180,6 @@ resource "aws_api_gateway_integration" "my_integration" {
   uri                     = aws_lambda_function.example_lambda_test.invoke_arn
 }
 
-resource "aws_api_gateway_method" "my_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.my_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_authorizer" "my_authorizer" {
   name                   = "my_authorizer"
   rest_api_id            = aws_api_gateway_rest_api.my_api.id
