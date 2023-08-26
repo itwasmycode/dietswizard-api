ef58457dd9555d868e513872513d66321ab4af5d
itwasmycode <kemalemrecolak@gmail.com>
2023-08-26 22:52:10 +0300

    aws proxy added and input, output has been changed.

--- register-api/src/main/scala/LambdaHandler.scala
+++ register-api/src/main/scala/LambdaHandler.scala
@@ -1,5 +1,11 @@
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import java.util.{Map => JavaMap, HashMap => JavaHashMap}
+import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+import java.io.{InputStream, OutputStream}
+import java.nio.charset.StandardCharsets
+
+import java.util.{Base64,Map => JavaMap, HashMap => JavaHashMap}
 import org.slf4j.LoggerFactory
 import scala.concurrent.{ExecutionContext, Await}
 import scala.concurrent.duration._
@@ -10,13 +16,18 @@ import java.util.UUID
 import at.favre.lib.crypto.bcrypt._
 
 class LambdaHandler() extends RequestHandler[JavaMap[String, String], JavaMap[String, String]] {
-
   val logger = LoggerFactory.getLogger(getClass)
   implicit val ec = ExecutionContext.global
+  val objectMapper = new ObjectMapper().registerModule(DefaultScalaModule)
 
-  override def handleRequest(input: JavaMap[String, String], context: Context): JavaMap[String, String] = {
-    val email = input.get("email")
-    val password = input.get("password")
+  override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
+    val event = objectMapper.readValue(input, classOf[APIGatewayProxyEvent])
+    val body = new String(Base64.getDecoder.decode(event.body), StandardCharsets.UTF_8)
+    val request = objectMapper.readValue(body, classOf[Request])
+    val response = new JavaHashMap[String, String]()
+
+    val email = request.email
+    val password = request.password
 
     if (!InformationValidator.validateEmail(email) || !InformationValidator.validatePassword(password)) {
       return new JavaHashMap[String, String] {
@@ -35,7 +46,7 @@ class LambdaHandler() extends RequestHandler[JavaMap[String, String], JavaMap[St
             }
           case Left(_) =>
             val hashedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray)
-            val newUser = DatabaseHandler.User(UUID.randomUUID().toString, email, hashedPassword)
+            val newUser = DatabaseHandler.User(UUID.randomUUID(), email, hashedPassword)
             Await.result(DatabaseHandler.createUser(newUser)(db, ec), Duration.Inf) match {
               case Right(_) =>
                 new JavaHashMap[String, String] {
