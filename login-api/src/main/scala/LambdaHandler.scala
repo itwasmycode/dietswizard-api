92c8687b26f876d42bc20795c2e7b0e89d97e892
itwasmycode <kemalemrecolak@gmail.com>
2023-09-01 16:27:51 +0300

    re-creating whole infrastructure.2

--- login-api/src/main/scala/LambdaHandler.scala
+++ login-api/src/main/scala/LambdaHandler.scala
@@ -53,7 +53,7 @@ object LambdaHandler extends RequestHandler[APIGatewayProxyRequestEvent,APIGatew
               val result = Await.result (DatabaseHandler.authenticateUser (email, password, refreshToken, expireDate) (db, ec), Duration.Inf)
               result match {
               case Right (user) =>
-                TokenHandler.createJwtToken (email, secret, "dietswizard", "dietswizard") match {
+                TokenHandler.createJwtToken (email,user.id, secret, "dietswizard", "dietswizard") match {
                 case Success (accessToken) =>
                     val responseBody = Map ("accessToken" -> accessToken.toString, "refreshToken" -> refreshToken.toString).asJava
                     return new APIGatewayProxyResponseEvent ()
