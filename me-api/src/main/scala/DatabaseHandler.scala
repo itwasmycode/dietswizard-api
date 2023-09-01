57ce66e1177801a3ac08f78207d8ed0bb45dd81c
itwasmycode <kemalemrecolak@gmail.com>
2023-09-02 00:43:30 +0300

    re-creating whole infrastructure.16

--- me-api/src/main/scala/DatabaseHandler.scala
+++ me-api/src/main/scala/DatabaseHandler.scala
@@ -18,16 +18,24 @@ object DatabaseHandler {
 
   class Users(tag: Tag) extends Table[User](tag, "users") {
     def user_id = column[Int]("user_id", O.PrimaryKey)
+
     def email = column[String]("email")
+
     def passwordHash = column[String]("password")
+
     def uuid = column[String]("uuid")
+
     def gender = column[Option[Int]]("gender")
+
     def birthday = column[Option[Date]]("birthday")
+
     def premium = column[Boolean]("premium")
+
     def status = column[Int]("status")
+
     def * = (
       user_id, email, passwordHash, uuid,
-      gender.?, birthday.?, premium, status
+      gender, birthday, premium, status
     ) <> (User.tupled, User.unapply)
   }
 
